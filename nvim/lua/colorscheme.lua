1a0783a46c042c28e937aa3e0a24a2cc8390f6fa
gilbert12 <gilbert12.tw@gmail.com>
2026-05-24 02:24:03 +0800

    update colorscheme

--- nvim/lua/colorscheme.lua
+++ nvim/lua/colorscheme.lua
@@ -3,6 +3,14 @@ require("catppuccin").setup({
     transparent_background = true,
     styles = {
         comments = {},
+        conditionals = {},
+        functions = {},
+        keywords = {},
+        loops = {},
+        operators = {},
+        properties = {},
+        types = {},
+        variables = {},
     },
     integrations = {
         mini = {
@@ -30,15 +38,8 @@ for _, group in ipairs(transparent_groups) do
     vim.api.nvim_set_hl(0, group, { bg = "NONE" })
 end
 
-local no_italic_groups = {
-    "Comment",
-    "@comment",
-    "@comment.documentation",
-}
-
-for _, group in ipairs(no_italic_groups) do
-    local ok, hl = pcall(vim.api.nvim_get_hl, 0, { name = group, link = false })
-    if ok and next(hl) ~= nil then
+for group, hl in pairs(vim.api.nvim_get_hl(0, {})) do
+    if hl.italic then
         hl.italic = false
         vim.api.nvim_set_hl(0, group, hl)
     end
