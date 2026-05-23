4ef69b1e55080a3af3d159b3cc4d2f636faeba30
gilbert12 <gilbert12.tw@gmail.com>
2026-05-24 01:06:55 +0800

    update lsp config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -2,12 +2,60 @@ require("mason").setup()
 
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>rf", vim.lsp.buf.format, { desc = "Format Local buffer" })
-vim.keymap.set("n", "df", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
 
 vim.diagnostic.config({
     virtual_text = true,
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = "",
+            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.INFO] = "",
+            [vim.diagnostic.severity.HINT] = "󰌵",
+        },
+    },
     underline = false,
     update_in_insert = false,
+    severity_sort = true,
+})
+
+local diagnostic_cache = {}
+local show_diagnostics = {}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = function(err, params, ctx)
+    if err ~= nil or params == nil or params.uri == nil then
+        return
+    end
+
+    local bufnr = vim.fn.bufnr(vim.uri_to_fname(params.uri))
+    if bufnr == -1 then
+        return
+    end
+
+    diagnostic_cache[bufnr] = diagnostic_cache[bufnr] or {}
+    diagnostic_cache[bufnr][ctx.client_id] = params
+
+    if show_diagnostics[bufnr] then
+        vim.lsp.diagnostic.on_publish_diagnostics(err, params, ctx)
+    end
+end
+
+vim.api.nvim_create_autocmd({ "InsertEnter", "TextChanged", "TextChangedI" }, {
+    desc = "Hide LSP diagnostics while editing",
+    callback = function(args)
+        show_diagnostics[args.buf] = false
+        vim.diagnostic.reset(nil, args.buf)
+    end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePost", {
+    desc = "Publish LSP diagnostics after saving",
+    callback = function(args)
+        show_diagnostics[args.buf] = true
+
+        for client_id, params in pairs(diagnostic_cache[args.buf] or {}) do
+            vim.lsp.diagnostic.on_publish_diagnostics(nil, params, { client_id = client_id })
+        end
+    end,
 })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
--- nvim/lua/pack.lua
+++ nvim/lua/pack.lua
@@ -2,16 +2,19 @@ vim.pack.add({
     { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
     "https://github.com/nvim-mini/mini.nvim",
     "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/nvim-tree/nvim-web-devicons",
     "https://github.com/rafamadriz/friendly-snippets",
     { src = "https://github.com/nvim-treesitter/nvim-treesitter", branch = "main" },
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/mason-org/mason.nvim",
+    "https://github.com/stevearc/aerial.nvim",
     { src = "https://github.com/ThePrimeagen/harpoon", version = "harpoon2" },
     "https://github.com/tpope/vim-fugitive",
 })
 
 require("colorscheme")
 require("plugins.harpoon")
+require("plugins.aerial")
 
 -- mini files ----
 local MiniFiles = require("mini.files")
